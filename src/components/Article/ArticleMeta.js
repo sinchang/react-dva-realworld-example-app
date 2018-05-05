08d105e2b06c6715868cc8905fd846b9a90e99a6
sinchang <sinchangwen@gmail.com>
2018-05-05 15:59:55 +0800

    chore: remove DEFAULT_AVATAR_IMAGE

--- src/components/Article/ArticleMeta.js
+++ src/components/Article/ArticleMeta.js
@@ -1,7 +1,6 @@
 import ArticleActions from './ArticleActions';
 import { Link } from 'dva/router';
 import React from 'react';
-const DEFAULT_AVATAR_IMAGE = 'https://static.productionready.io/images/smiley-cyrus.jpg'
 const ArticleMeta = props => {
   const article = props.article;
   const profile = props.profile;
@@ -9,7 +8,7 @@ const ArticleMeta = props => {
   return (
     <div className="article-meta">
       <Link to={`/@${article.author.username}`}>
-        <img src={article.author.image ? article.author.image : DEFAULT_AVATAR_IMAGE} alt={article.author.username} />
+        <img src={article.author.image} alt={article.author.username} />
       </Link>
 
       <div className="info">
